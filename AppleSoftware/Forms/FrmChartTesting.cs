ff4a773b32eec3359cdaf03cb20d41700ccb4fb7
Mauricio Gonzalez <gonzalezmoralesmauricioo@gmail.com>
2022-10-28 09:35:36 -0600

    Segundo Diseño

---
+++ AppleSoftware/Forms/FrmChartTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppleSoftware.Forms
+{
+    public partial class FrmChartTesting : Form
+    {
+        public FrmChartTesting()
+        {
+            InitializeComponent();
+        }
+    }
+}
